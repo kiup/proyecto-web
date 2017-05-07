0b07c7078c6a3f86d6bb5293625ffaef475b5d4d
Unknown <angelbastogz@gmail.com>
2017-05-07 13:54:59 -0500

    Primeros cambios para php
    Se crearon carpetas para guardar el header y el footer, de esa forma podemos reutilizar el codigo utilizando php.

---
+++ includes/templates/footer.php
@@ -0,0 +1,42 @@
+<!--FOOTER-->
+<footer class="site-footer">
+    <div class="contenedor clearfix">
+        <div class="footer-informacion">
+            <h3>Sobre <span>cityconferences</span></h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="ultimos-tweets">
+            <h3>&Uacute;ltimos <span>tweets</span></h3>
+            <ul>
+                <li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                    nisi ut aliquip ex ea commodo consequat.</li>
+                <li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                    nisi ut aliquip ex ea commodo consequat.</li>
+                <li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                    nisi ut aliquip ex ea commodo consequat.</li>
+            </ul>
+        </div>
+        <div class="menu">
+            <h3>Redes <span>sociales</span></h3>
+            <nav class="redes-sociales">
+                <a href="#"><img src="imagenes/fa-facebook-square.png" id="facebook-footer" width="50" height="50" alt="icono-facebook"></a>
+                <a href="#"><img src="imagenes/fa-twitter.png" id="twitter-footer" width="50" height="50" alt="icono-twitter"></a>
+                <a href="#"><img src="imagenes/fa-instagram.png" id="instagram-footer" width="50" height="50" alt="icono-instagram"></a>
+            </nav>
+        </div>
+    </div>
+    <p class="copyright">Con &hearts; por el equipo # de la materia de web</p>
+</footer>
+<!--FOOTER-->
+
+<!--API-MAPA-->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAI9clEUB-BHFbI_ICkdmVdGF2WADwsy7E&callback=initMap" async defer>
+</script>
+</body>
+</html>
---
+++ includes/templates/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="css/main.css" rel="stylesheet" type="text/css">
+    <link href="css/testimoniales.css" rel="stylesheet" type="text/css">
+    <link href="css/registro.css" rel="stylesheet" type="text/css">
+    <link href="css/newsletter.css" rel="stylesheet" type="text/css">
+    <link href="css/footer.css" rel="stylesheet" type="text/css">
+    <script src="mapa.js"></script>
+    <meta name="viewport" content="width=device-width" />
+    <meta charset="UTF-8">
+</head>
+<body>
+<div class="hero">
+    <div class="contenido-header"><!--Barra redes sociales-->
+        <nav class="redes-sociales">
+            <a href="#"><img src="imagenes/fa-facebook-square.png" id="facebook" width="50" height="50" alt="icono-facebook"></a>
+            <a href="#"><img src="imagenes/fa-twitter.png" id="twitter" width="50" height="50" alt="icono-twitter"></a>
+            <a href="#"><img src="imagenes/fa-instagram.png" id="instagram" width="50" height="50" alt="icono-instagram"></a>
+        </nav>
+        <div class="informacion-evento"><!--Información evento-->
+            <div class="clearfix">
+                <p class="fecha"><img src="imagenes/fa-calendar.png" width="23" height="23" alt="icono-instagram">10-Dic-17</p>
+                <p class="lugar"><img src="imagenes/fa-map-marker.png" width="20" height="22" alt="icono-instagram">M&eacute;rida, Yucat&aacute;n</p>
+            </div>
+            <h1 class="nombre-sitio">CityConferences</h1>
+            <p class="slogan">La mejor p&aacute;gina de <span>Conferencias</span></p>
+        </div>
+    </div>
+
+</div>
+
+<div class="barra">
+    <div class="contenedor clearfix">
+        <div class="logo">
+            <img src="" alt="logo-cityconferences">
+        </div>
+
+        <div class="menu-movil">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+
+        <nav class="navegacion-principal clearfix">
+            <a href="#">Conferencias</a>
+            <a href="#">Calendario</a>
+            <a href="#">Invitados</a>
+            <a href="registro.php">Reservaciones</a>
+        </nav>
+    </div><!--contenedor-->
+</div><!--barra-->
