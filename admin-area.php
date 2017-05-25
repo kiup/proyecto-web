58086379e1a610e3124f42a0d891e491460f0c63
Milka Cabrilla <milka.cabrilla@gmail.com>
2017-05-25 09:15:12 -0500

    Update admin-area.php

--- admin-area.php
+++ admin-area.php
@@ -3,7 +3,7 @@
     usuario_admin_autenticado();
 ?>
 
-<?php include_once 'includes/templates/header.php';?>
+<?php include_once 'includes/templates/barra.php';?>
 
     <header>
         <link href="css/admin.css" rel="stylesheet" type="text/css">
