b4e84991c5b679ce07ec65b5f810b49de6d4e3ca
Milka Cabrilla <milka.cabrilla@gmail.com>
2017-05-25 09:05:44 -0500

    Update header.php

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -33,26 +33,3 @@
     </div>
 
 </div>
-
-<div class="barra">
-    <div class="contenedor clearfix">
-        <div class="logo">
-          <a href="index.php">
-            <img src="imagenes/CityConferences.jpg" alt="logo-cityconferences">
-          </a>
-        </div>
-
-        <div class="menu-movil">
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
-
-        <nav class="navegacion-principal clearfix">
-            <a href="index.php#seccion-programa">Conferencias</a>
-            <a href="calendario.php#seccion-calendario">Calendario</a>
-            <a href="login.php">Iniciar</a>
-            <a href="registro.php#seccion-registro">Reservaciones</a>
-        </nav>
-    </div><!--contenedor-->
-</div><!--barra-->
